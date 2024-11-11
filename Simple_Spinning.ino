0bcd345d2a99383e9c8c122852e6c97b5e268c7f
ClemensPhillipp <73097960+ClemensPhillipp@users.noreply.github.com>
2024-11-11 21:39:33 +0100

    Add files via upload

---
+++ Simple_Spinning.ino
@@ -0,0 +1,64 @@
+#include <Servo.h>
+int GSM1 = 3;
+
+// ******Parameter******
+int forward = true;
+int reverse = true;
+int maxspeed = 50;
+int spinning_time = 10000;
+int pause_time = 3000;
+// ******Parameter******
+
+#define In1 5
+#define In2 6
+
+void setup() {
+  pinMode(In1, OUTPUT);
+  pinMode(In2, OUTPUT);
+  pinMode(GSM1, OUTPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+  if (forward) {
+    startSpin();
+    delay(spinning_time);
+  }
+  stopSpin();
+  if (reverse) {
+    reverseSpin();
+    delay(spinning_time);
+    stopSpin();
+  }
+  delay(pause_time);
+}
+
+void startSpin() {
+  digitalWrite(In1, HIGH);
+  digitalWrite(In2, LOW);
+  for (int i = 0; i <= maxspeed; i += 1) {
+    analogWrite(GSM1, i);
+    Serial.println(i);
+    delay(50);
+  }
+}
+
+void reverseSpin() {
+  digitalWrite(In1, LOW);
+  digitalWrite(In2, HIGH);
+  for (int i = 0; i <= maxspeed; i += 1) {
+    analogWrite(GSM1, i);
+    Serial.println(i);
+
+    delay(50);
+  }
+}
+
+void stopSpin() {
+  for (int i = maxspeed; i >= 0; i -= 1) {
+    analogWrite(GSM1, i);
+    delay(50);
+  }
+  digitalWrite(In1, LOW);
+  digitalWrite(In2, LOW);
+}
